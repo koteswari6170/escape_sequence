cfb00b30952116837f4983e1f3509b50cb245d3a
koteswari6170 <102402089+koteswari6170@users.noreply.github.com>
2022-03-26 17:54:57 +0530

    Add files via upload

---
+++ sequence.py
@@ -0,0 +1,87 @@
+# print('hello\nworld') #hello nextline world
+# print('hello pyt\'hon') #hello pyt'hon
+# print("hello py\"thon") #hello py"thon
+# print("F:\Uatch_9_15_AM") #syntaxerror
+# print("F:\\Uatch_9_15_AM")  #F:\Uatch_9_15_AM
+# print("C:\\Users\\JOSH_HD")#C:\Users\JOSH_HD
+# print("C:\\Users\\JOSH\\Downloads") #|Users\JOSH\Downloads
+
+# print('\u0001') #☻
+# print('\u0002') #♥
+# print('\u0003') #♦
+# print('\u0004') #♣
+# print('\u0005') #♠
+# print('\u0006') #blankline
+# print('\u0007') #blankline
+# print('\u0008') #blankline
+# print('\u0009') #blankline
+# print('\U0001f601') #😁
+# print('\U0001f602') #😂
+# print('\U0001f603') #😃
+# print('\U0001f604') #😄
+
+
+# print('https://www.pythonanywhere.com/betteries_included/') #https://www.pythonanywhere.com/betteries_included/
+# print("C:\\Users\\JOSH\\Desktop\\915AM") #\Users\JOSH\Desktop\915AM
+
+# print("hello \bworld") #helloworld
+# print("hello\bworld") #hellworld
+# print("hello\\bworld") #hello\bworld
+
+# print("hello\tworld") #hello   world
+# print("hello   world")#hello   world
+# print("\thelloworld")       # helloworld
+# print("hello\t" "world") #hello   world
+# print("hello   world") #hello   world
+
+# print("py\ron") #on
+# print("pyth\ron") #onth
+# print("pythpy\ron") #onthpy
+# print("hello\rpy ") #pn lo
+
+# print("My Name is \r Allu Arjun") # Allu Arjun
+
+# print("hello\ankith") #hellonkith
+# print("hello\aankith") #helloankith
+# print("hello\\ankith") #hello\ankith
+# print("hello\bnkith") #hellnkith
+# print("hello\cnkith") #hello\cnkith
+# print("hello\dnkith") #hello\dnkith
+# print("hello\enkith") #hello\enkith
+# print("hello\\fun") #hello\fun
+# print("hello\gnkith") #hello\gnkith
+# print("hello\hnkith") #hello\hnkith
+# print("hello\inkith") #hello\inkith
+# print("hello\jnkith") #hello\jnkith
+# print("hello\knkith") #hello\knkith
+# print("hello\lnkith") #hello\lnkith
+# print("hello\mnkith") #hello\mnkith
+# print("hello\nnkith") #hello nextline nkith
+# print("hello\onkith") #hello\knkith
+# print("hello\pnkith") #hello\pnkith
+# print("hello\qnkith") #hello\qknkith
+# print("hello\rnkith") #nkith
+# print("hello\snkith") #hello\snkith
+
+# print("\thellonkith") #        hellonkith
+# print("hello\tnkith") #hello   nkith
+# print("hello \tnkith")#hello   nkith
+# print("hello   nkith")#hello   nkith
+# print("hello    \tnkith") #hello        nkith
+# print("hello\t nkith") #hello   nkith
+# print("Hello   nkith") #hello   nkith
+# print("hello\unkith") #syntaxerror
+# print("hello\\unkith") #hello\unkith
+# print("hello\vnkith") #hello\vnkith
+# print("hello\wnkith") #hello\wnkith
+# print("hello\\xavier") #hello\xavier
+# print("hello\ynkith") #hello\ynkith
+# print("hello\znkith") #hello\znkith
+
+# print("Hello\Umberilla") #syntaxerror
+# print("Hello\Nikil") #syntaxerror
+
+
+
+
+
